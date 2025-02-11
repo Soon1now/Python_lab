8178805c4a6846db41cc3c8b1b34219be4443dba
Soon1now <sofa.147@mail.ru>
2025-02-11 18:25:14 +0300

    Задание 6
    Дана строка. Необходимо подсчитать количество чисел в этой строке, значение которых меньше 5

---
+++ Lab_1/Task_6.py
@@ -0,0 +1,9 @@
+# Дана строка. Необходимо подсчитать количество чисел в этой строке,
+# значение которых меньше 5
+def cnt_number(text):
+    cnt = sum(1 for i in text if int(i)<5)
+    return cnt
+
+str = "1338025998428363"
+result = cnt_number(str)
+print(result)
