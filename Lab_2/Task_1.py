016e3d5b38d4812ef8d870673d1f8b75b4c84bbb
Soon1now <sofa.147@mail.ru>
2025-02-15 19:08:54 +0300

    Задание 1. Полное решение

--- Lab_2/Task_1.py
+++ Lab_2/Task_1.py
@@ -1,3 +1,7 @@
+n = int(input("Введите максимальное число: "))
+possible_numbers = set(range(1, n + 1))
+
+
 def respond_to_question(question, possible_numbers):
 
     question_numbers = set(map(int, question.split()))
@@ -9,3 +13,22 @@ def respond_to_question(question, possible_numbers):
         return "YES"
     else:
         return "NO"
+
+while True:
+    question = input("Введите вопрос (или HELP для завершения): ")
+    if question == "HELP":
+        break
+
+    response = respond_to_question(question, possible_numbers)
+    print(response)
+
+    question_numbers = set(map(int, question.split()))
+
+    if response == "YES":
+        possible_numbers.intersection_update(question_numbers)
+    elif response == "NO":
+        possible_numbers.difference_update(question_numbers)
+    print(f"Возможные числа: {possible_numbers}")
+
+remaining_numbers_sorted = sorted(possible_numbers)
+print(" ".join(map(str, remaining_numbers_sorted)))
